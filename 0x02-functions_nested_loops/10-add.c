31006f1d7b098c3cbdc2caab1ee80dd42b85eb29
PantheraNestah <pantheranestah@gmail.com>
2023-06-20 16:49:17 -0700

    C functions programming

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - adds two integers passed as arguments
+ * @i: first integer argument passed to add function
+ * @j: second integer argument passed to add function
+ * Return: value of sum of i and j
+ */
+int add(int i, int j)
+{
+	return (i + j);
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit of a number
+ * @i: integer argument for the function print_last_digit function
+ * Return: value of i's last digit
+ */
+int print_last_digit(int i)
+{
+	int last = i % 10;
+
+	_putchar(last + '0');
+	return (last);
+}
