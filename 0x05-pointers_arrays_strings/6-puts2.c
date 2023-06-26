e7f8720252522888d39492895cae2fe5f102be3d
PantheraNestah <pantheranestah@gmail.com>
2023-06-26 13:06:25 -0700

    Corrections

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,13 +1,14 @@
 #include "main.h"
+#include <string.h>
 /**
  * puts2 - prints every character of a string
  * @str: pointer to string whose characters are printed
  */
 void puts2(char *str)
 {
-	int i = 0;
+	int i = 0, len = (int)strlen(str) - 1;
 
-	while (str[i])
+	while (i < len)
 	{
 		_putchar(str[i]);
 		i = i + 2;
--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,12 +6,18 @@
  */
 void puts_half(char *str)
 {
-	int len = (int)strlen(str), midpoint, i;
+	int len = (int)strlen(str) - 1, midpoint, i;
 
 	if ((len % 2) == 0)
+	{
 		midpoint = len / 2;
+		midpoint += 1;
+	}
 	else
+	{
 		midpoint = (len - 1) / 2;
+		midpoint += 1;
+	}
 	i = midpoint;
 	while (str[i])
 	{
