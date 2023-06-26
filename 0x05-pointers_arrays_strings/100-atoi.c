941eacffdca993cd79f17b1e25fcd195ebbbddab
PantheraNestah <pantheranestah@gmail.com>
2023-06-26 14:52:24 -0700

    corrections

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,7 +9,7 @@ int _atoi(char *s)
 	int i = 0, sn = 1;
 	unsigned int val = 0;
 
-	while(!(s[i] <= '9' && s[i] >= '0') && s[i] != '\0')
+	while (!(s[i] <= '9' && s[i] >= '0') && s[i] != '\0')
 	{
 		if (s[i] == '-')
 			sn *= -1;
