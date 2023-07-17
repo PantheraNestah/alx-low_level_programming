f21acec02634d30f12f080cb127396c60a18ccd0
PantheraNestah <pantheranestah@gmail.com>
2023-07-17 12:37:49 -0700

    syntax correction on 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -18,10 +18,7 @@ void print_dog(struct dog *d)
 			printf("Name: (nil)\n");
 		else
 			printf("Name: %s\n", d->name);
-		if (d->age)
-			printf("Age: %.6f\n", d->age);
-		else
-			printf("Age: (nil)");
+		printf("Age: %.6f\n", d->age);
 		if (d->owner == NULL)
 			printf("Owner: (nil)\n");
 		else
