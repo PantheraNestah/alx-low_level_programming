b8484aa33590dd294d10ea56af18e77da72df204
PantheraNestah <pantheranestah@gmail.com>
2023-07-17 14:57:39 -0700

    first commit on 0x0D-preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#ifndef THIS_FILE
+#define THIS_FILE __FILE__
+
+/**
+ * main - entry point
+ * Return: 0(success)
+ */
+int main(void)
+{
+	printf("%s\n", THIS_FILE);
+	return (0);
+}
+#endif
---
+++ 0x0D-preprocessor/testmain.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "3-function_like_macro.h"
+#include "3-function_like_macro.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    printf("%d\n", ABS(98));
+    printf("%d\n", ABS(-98));
+    return (0);
+}
